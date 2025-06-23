7ce8237ff8cabc6ab99dc99210cfeb6ecfd979da
Lorenzo Quinti <lorenzo.quinti@dexma.it>
2025-06-23 19:09:03 +0200

    UPDATE: find and replace

--- src/DocumentEditor/RecicleScrollerEditor.vue
+++ src/DocumentEditor/RecicleScrollerEditor.vue
@@ -1,135 +1,202 @@
 <template>
   <div class="editor" ref="editor">
     <RecycleScroller
+      v-if="!loading"
       class="content"
       :items="pages"
       :item-size="pagePxHeight"
       page-mode
       key-field="uuid"
       :buffer="5"
-        :item-style="getItemStyle"
-
+      :item-style="getItemStyle"
+      emit-update
+      @update="handleScrollUpdate"
     >
       <template #default="{ item: page, index }">
         <div
           class="page"
-          :style="page_style(index, !page.template)"
+          :style="pageStyle(index, !page.template)"
           :contenteditable="editable && !page.template"
-          :data-content-idx="page.content_idx"
+          :data-content-idx="page.contentIdx"
+          @input="handleInput($event, page.contentIdx)"
         >
           <div
-            v-if="typeof content[page.content_idx] === 'string'"
-            v-html="content[page.content_idx] ?? ''"
+            v-if="typeof modelValue[page.contentIdx] === 'string'"
+            v-html="modelValue[page.contentIdx] ?? ''"
           />
           <component v-else :is="page.template" v-bind="page.props" />
         </div>
       </template>
     </RecycleScroller>
+
+    <FindAndReplaceModal
+      :pages="pages"
+      :renderedPageIndexes="renderedPageIndexes"
+      :content="modelValue"
+      :replace-active="true"
+      @onUpdate="handleUpdateFindEndReplace"
+    />
   </div>
 </template>
 
-<script>
-import { RecycleScroller } from 'vue-virtual-scroller'
-import 'vue-virtual-scroller/dist/vue-virtual-scroller.css'
+<script setup>
+import { ref, computed, watch, defineProps, onMounted, nextTick } from "vue";
+import { RecycleScroller } from "vue-virtual-scroller";
+import "vue-virtual-scroller/dist/vue-virtual-scroller.css";
+import FindAndReplaceModal from "./FindAndReplaceModal.vue";
 
-export default {
-  name: 'VirtualScrollerEditor',
-  components: {
-    RecycleScroller
-  },
-  props: {
-    content: {
-      type: Array,
-      required: true
-    },
-    editable: {
-      type: Boolean,
-      default: true
-    },
-    page_format_mm: {
-      type: Array,
-      default: () => [210, 297]
-    },
-    page_margins: {
-      type: [String, Function],
-      default: '10mm 15mm'
-    },
-    zoom: {
-      type: Number,
-      default: 1.0
-    },
-    display: {
-      type: String,
-      default: 'grid'
-    }
-  },
-  data() {
-    return {
-      pages: [],
-      pageGap: 24 
-    }
-  },
-  computed: {
-    pagePxHeight() {
-      return (this.page_format_mm[1] / 0.2645833333333) * this.zoom
-    }
-  },
-  watch: {
-    content: {
-      immediate: true,
-      handler(newContent) {
-        this.pages = newContent.map((c, idx) => ({
-          uuid: Math.random().toString(36).slice(-5),
-          content_idx: idx,
-          template: c.template,
-          props: c.props
-        }))
-      }
-    }
-  },
-  methods: {
-    page_style(index, allowOverflow) {
-      const pxInMm = 0.2645833333333
-      const widthPx = this.page_format_mm[0] / pxInMm
-      const heightPx = this.page_format_mm[1] / pxInMm
+const emit = defineEmits(["update:modelValue"]);
 
-      const style = {
-        width: this.page_format_mm[0] + 'mm',
-        transform: `scale(${this.zoom})`,
-        padding: typeof this.page_margins === 'function'
-          ? this.page_margins(index + 1, this.pages.length)
-          : this.page_margins
-      }
-      style[allowOverflow ? 'minHeight' : 'height'] = this.page_format_mm[1] + 'mm'
-      return style
-    },
-
-    getItemStyle() {
-    return {
-      marginBottom: this.pageGap + 'px',
-      display: 'flex',
-      justifyContent: 'center'
-    }
+const props = defineProps({
+  modelValue: {
+    type: Array,
+    required: true,
   },
+  editable: {
+    type: Boolean,
+    default: true,
+  },
+  page_format_mm: {
+    type: Array,
+    default: () => [210, 297],
+  },
+  page_margins: {
+    type: [String, Function],
+    default: "10mm 15mm",
+  },
+  zoom: {
+    type: Number,
+    default: 1.0,
+  },
+  display: {
+    type: String,
+    default: "grid",
+  },
+});
+
+const loading = ref(false);
+const editorKey = ref(0);
+const pages = ref([]);
+const pageGap = 24;
+const renderedPageIndexes = ref([]);
+
+const pagePxHeight = computed(
+  () => (props.page_format_mm[1] / 0.2645833333333) * props.zoom
+);
+
+const handleGeneratePages = () => {
+  loading.value = true;
+  try {
+    pages.value = props.modelValue.map((c, idx) => ({
+      uuid: Math.random().toString(36).slice(-5),
+      contentIdx: idx,
+      template: c.template,
+      props: c.props,
+    }));
+  } finally {
+    loading.value = false;
   }
-}
+};
+
+const pageStyle = (index, allowOverflow) => {
+  const style = {
+    width: `${props.page_format_mm[0]}mm`,
+    transform: `scale(${props.zoom})`,
+    padding:
+      typeof props.page_margins === "function"
+        ? props.page_margins(index + 1, pages.value.length)
+        : props.page_margins,
+  };
+
+  if (allowOverflow) {
+    style.minHeight = `${props.page_format_mm[1]}mm`;
+  } else {
+    style.height = `${props.page_format_mm[1]}mm`;
+  }
+  return style;
+};
+
+const getItemStyle = () => {
+  return {
+    marginBottom: `${pageGap}px`,
+    display: "flex",
+    justifyContent: "center",
+  };
+};
+
+const handleScrollUpdate = (
+  startIndex,
+  endIndex,
+  visibleStartIndex,
+  visibleEndIndex
+) => {
+  const indexes = [];
+  for (let i = startIndex; i <= endIndex; i++) {
+    indexes.push(i);
+  }
+  renderedPageIndexes.value = indexes;
+};
+
+const handleUpdateFindEndReplace = (updatedContent) => {
+  loading.value = true;
+  try {
+    // pages.value = updatedPages;
+    // editorKey.value += 1;
+    // const updatedContent = [...props.modelValue];
+    // for (const page of updatedPages) {
+    //   if (
+    //     !page.template &&
+    //     typeof updatedContent[page.contentIdx] === "string"
+    //   ) {
+    //     const el = document.querySelector(
+    //       `[data-content-idx="${page.contentIdx}"]`
+    //     );
+    //     if (el) {
+    //       updatedContent[page.contentIdx] = el.innerHTML;
+    //     }
+    //   }
+    // }
+
+    emit("update:modelValue", updatedContent);
+  } finally {
+    loading.value = false;
+  }
+};
+
+// NON FUNZIONA
+const handleInput = (event, contentIdx) => {
+  const newContent = [...props.modelValue];
+  newContent[contentIdx] = event.target.innerHTML;
+  emit("update:modelValue", newContent);
+};
+
+onMounted(() => {
+  handleGeneratePages();
+});
+
+watch(
+  () => props.modelValue,
+  () => {
+    handleGeneratePages();
+  },
+  { immediate: true, deep: true }
+);
 </script>
 
 <style scoped>
 .editor {
   display: block;
+  position: relative;
 }
 .content {
-  position: relative;
   outline: none;
   margin: 0;
   padding: 0;
   min-width: 100%;
 }
 .page {
-  position: absolute;
   box-sizing: border-box;
-  left: 50%;
+  margin: 0 auto;
   transform-origin: center top;
   background: white;
   box-shadow: 0 1px 3px rgba(60, 64, 67, 0.15);
---
+++ src/DocumentEditor/utils.js
@@ -0,0 +1,44 @@
+export const debounce = (fn, delay) => {
+  let timer;
+
+  return (...args) => {
+    clearTimeout(timer);
+
+    timer = setTimeout(() => {
+      fn(...args);
+    }, delay);
+  };
+};
+
+export const getTestContent = () => {
+  const textstring = `<h1>Hello world!</h1><p>This is a rich-text editor built on top of <span contenteditable="false"><a href="https://vuejs.org/" target="_blank">Vue.js</a></span> using the native <span contenteditable="false"><a href="https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/Editable_content" target="_blank"><i>contenteditable</i></a></span> browser implementation and some JavaScript trickery to spread content over paper-sized pages.</p><p>Built-in functionality includes:</p><ul><li>Using Vue.js components as interactive page templates (see next page)</li><li>Word-by-word page splitting (<u>still experimental - only for plain HTML content</u>)</li><li>Native Print compatible</li><li>Dynamic document format and margins in millimeters</li><li>Custom page overlays (headers, footers, page numbers)</li><li>Page breaks</li><li>Smart zoom and page display modes</li><li>Computes text style at caret position</li></ul><p>This library may be useful if you design an application that generate documents and you would let the user to modify them slightly before printing / saving, but with limited / interactive possibilities. It does not intend to replace a proper document editor with full functionality.<br>Make sure this project is suitable to your needs before using it.</p><p>This demo adds:</p><ul><li>The top bar (<span contenteditable="false"><a href="https://github.com/motla/vue-file-toolbar-menu" target="_blank">vue-file-toolbar-menu</a></span> component) and the functions associated with it</li><li>Rewritten history stack (undo/redo) compatible with native commands</li><li>Pinch and trackpad zooming</li></ul><p>Check out the <span contenteditable="false"><a href="https://github.com/motla/vue-document-editor/blob/master/src/Demo/Demo.vue" target="_blank">Demo.vue</a></span> file if you need to add these functionalities to your application.</p><p>The link below is an example of non-editable block set with <code>contenteditable="false"</code>:</p><p style="text-align:center" contenteditable="false"><a href="https://github.com/motla/vue-document-editor">View docs on Github</a>, you can't edit me.</p><p>But you can still edit this.</p>`;
+
+  const content = new Array(1000).fill(textstring);
+  return content;
+};
+
+export function replaceAllMatches(content, matches, replaceText) {
+    if (!replaceText || !matches.length) return content
+
+    const grouped = new Map()
+    for (const match of matches) {
+        if (!grouped.has(match.pageIdx)) grouped.set(match.pageIdx, [])
+        grouped.get(match.pageIdx).push({ start: match.start, end: match.end })
+    }
+
+    const newContent = [...content]
+
+    for (const [pageIdx, group] of grouped.entries()) {
+        let text = newContent[pageIdx]
+        let offset = 0
+        for (const { start, end } of group) {
+            const realStart = start + offset
+            const realEnd = end + offset
+            text = text.slice(0, realStart) + replaceText + text.slice(realEnd)
+            offset += replaceText.length - (end - start)
+        }
+        newContent[pageIdx] = text
+    }
+
+    return newContent
+}
